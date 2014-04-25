c5e1b9e9a1312a34578c7a367cd16e9f961f19ec
Hector D. Felix <hdfelix@gmail.com>
2014-04-25 13:23:11 -0700

    first commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocitoff::Application.config.secret_key_base = 'd15c2d2d79c5f73914684169f6055ba3547eed87093d52023a8e4725a850535a54dfc28275b099db337620f69d8c627cd25b945240f8e39a509b246188d92bbf'
