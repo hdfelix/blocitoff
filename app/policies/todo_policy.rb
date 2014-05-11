96a95c788e75c66765a1190b372f35b530fff0fa
Hector D. Felix <hdfelix@gmail.com>
2014-05-11 14:08:47 -0700

    Re-worked todo policy

--- app/policies/todo_policy.rb
+++ app/policies/todo_policy.rb
@@ -1,6 +1,18 @@
 class TodoPolicy < ApplicationPolicy
 
-	def destroy?
+	def index?
+		user.present?
+	end
+
+	def show?
 		user.present? && (record.user == user && record.user_id == user.id)
+	end
+	
+	def create?
+		show?
+	end
+
+	def destroy?
+		show?
 	end
 end
