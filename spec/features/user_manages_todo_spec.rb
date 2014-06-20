34cda47906f247acb458ebc1b406d881f760e04e
Hector D. Felix <hdfelix@gmail.com>
2014-06-19 17:43:13 -0700

    Updated script to capture real events

--- spec/features/user_manages_todo_spec.rb
+++ spec/features/user_manages_todo_spec.rb
@@ -1,49 +1,49 @@
 require 'spec_helper'
 
 feature 'User can manage Todo items' do
-	let(:authed_user) { 	
-		create_logged_in_user 
-	}
+  let(:authed_user) {     
+    create_logged_in_user 
+  }
 
-	scenario 'by accessing the Todo index' do
-		visit todos_path(authed_user)
-		expect(page).to have_content("My List")
-	end
+  scenario 'by accessing the Todo index' do
+    visit todos_path(authed_user)
+    expect(page).to have_content("My List")
+  end
 
-	scenario 'Can see the Todo list headers' do
-		visit todos_path(authed_user)
-		expect(page).to have_content('Description')
-		expect(page).to have_content('Days left')
-		expect(page).to have_content('Complete')
-	end
+  scenario 'Can see the Todo list headers' do
+    visit todos_path(authed_user)
+    expect(page).to have_content('Description')
+    expect(page).to have_content('Days left')
+    expect(page).to have_content('Complete')
+  end
 end
 
 feature 'User creates Todo item' do
-	let(:authed_user) { 	
-		@user = create_logged_in_user 
-		@todo = Todo.create(description: 'Test my app')
-	}
+  let(:authed_user) {     
+    @user = create_logged_in_user 
+    @todo = Todo.create(description: 'Test my app')
+  }
 
-	scenario 'Successfully' do
-		visit todos_path(authed_user)
-		expect{
-			fill_in 'todo_description', with: 'Meet up with the team'
-			click_button 'Save'
-		}.to change(Todo, :count).by (1)
-		expect(page).to have_content('Meet up with the team')
-		expect(page).to have_content('days')	
-	end
+  scenario 'Successfully' do
+    visit todos_path(authed_user)
+    expect{
+      fill_in 'todo_description', with: 'Meet up with the team'
+      click_button 'Save'
+    }.to change(Todo, :count).by (1)
+    expect(page).to have_content('Meet up with the team')
+    expect(page).to have_content('days')    
+  end
 
-	scenario 'sees \'SUCCESS\' message when saved' do
-		visit todos_path(authed_user)
-		fill_in 'Description', with: 'Meet up with the team'
-		click_button 'Save'
-		expect(page).to have_content('Your new TODO was saved.')
-	end
+  scenario 'sees \'SUCCESS\' message when saved' do
+    visit todos_path(authed_user)
+    fill_in 'Description', with: 'Meet up with the team'
+    click_button 'Save'
+    expect(page).to have_content('Your new TODO was saved.')
+  end
 
-	scenario 'invalid with \'Description\' missing' do
-		visit todos_path(authed_user)
-		expect{click_button 'Save'}.to change(Todo, :count).by (0)
-		expect(page).to have_content("can't be blank")
-	end
+  scenario 'invalid with \'Description\' missing' do
+    visit todos_path(authed_user)
+    expect{click_button 'Save'}.to change(Todo, :count).by (0)
+    expect(page).to have_content("can't be blank")
+  end
 end
--- spec/features/user_manages_users_spec.rb
+++ spec/features/user_manages_users_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 feature 'User accesses site' do
 
-	scenario 'And can see the home page' do
-		visit root_path
-		expect(page).to have_content('Blocitoff')
-	end
+  scenario 'And can see the home page' do
+    visit root_path
+    expect(page).to have_content('Blocitoff')
+  end
 
-	scenario 'And signs in' do
-		user = create(:user)
-		user.skip_confirmation!
-		user.save
-		
-		visit root_path
-		
-		click_link 'Sign in'
-		fill_in 'user_email', with: user.email
-		fill_in 'user_password', with: user.password
-		click_button 'Sign in'
-		expect(page).to have_content('Signed in successfully.')
-	end
+  scenario 'And signs in' do
+    user = create(:user)
+    user.skip_confirmation!
+    user.save
+
+    visit root_path
+
+    click_link 'Sign in'
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Sign in'
+    expect(page).to have_content('Signed in successfully.')
+  end
 end
